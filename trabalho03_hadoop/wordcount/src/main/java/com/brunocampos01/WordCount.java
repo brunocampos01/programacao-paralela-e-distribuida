1492a61b747d76f375a85afa7a0fa6db2a474d4e
Bruno Campos <brunocampos01@gmail.com>
2020-06-26 14:40:13 -0300

    Update and rename trabalho03_hadoop/wordcount/src/main/java/com/kleber/WordCount.java to trabalho03_hadoop/wordcount/src/main/java/com/brunocampos01/WordCount.java

--- trabalho03_hadoop/wordcount/src/main/java/com/brunocampos01/WordCount.java
+++ trabalho03_hadoop/wordcount/src/main/java/com/brunocampos01/WordCount.java
@@ -1,18 +1,16 @@
-package com.kleber;
+//https://hadoop.apache.org/docs/stable/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html
+
+package com.brunocampos01;
 
 import java.io.IOException;
 import java.util.*;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-//https://hadoop.apache.org/docs/stable/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html
 
 public class WordCount {
 
