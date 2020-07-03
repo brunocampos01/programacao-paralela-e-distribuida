b762e74d1d10ea803337b574e1c9d842aba36241
brunocampos01 <brunocampos01@gmail.com>
2020-07-02 23:09:23 -0300

    amended

--- trabalho02_openMP/lab2-tarefa-pratica.c
+++ trabalho02_openMP/lab2-tarefa-pratica.c
@@ -44,23 +44,29 @@ int main(int argc, char** argv)
 	clGetPlatformIDs(1, &platformId, NULL);
 	clGetDeviceIDs(platformId, CL_DEVICE_TYPE_GPU,
 	1, &deviceId, NULL);
+
 	/* Criação do contexto */
 	context = clCreateContext(0, 1, &deviceId,
 	NULL, NULL, NULL);
+
 	/* Criação da fila de comandos para o
 	dispositivo encontrado */
 	queue = clCreateCommandQueue(context, deviceId,
 	0, NULL);
+
 	/* Criação do objeto de programa a partir do
 	código-fonte armazenado na string source */
 	program = clCreateProgramWithSource(context, 1, &source,
 	NULL, NULL);
+
 	/* Compilação do programa para todos os
 	dispositivos do contexto */
 	clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+
 	/* Obtenção de um kernel a partir do
 	programa compilado */
 	kernel = clCreateKernel(program, "ArrayDiff", NULL);
+
 	/* Alocação e inicialização dos arrays no hospedeiro */
 	hostA = (int*) malloc(ARRAY_LENGTH * sizeof(int));
 	hostB = (int*) malloc(ARRAY_LENGTH * sizeof(int));
@@ -70,6 +76,7 @@ int main(int argc, char** argv)
 	hostA[i] = rand() % 101 - 50;
 	hostB[i] = rand() % 101 - 50;
 	}
+
 	/* Criação dos objetos de memória para comunicação com
 	a memória global do dispositivo encontrado */
 	bufA = clCreateBuffer(context, CL_MEM_READ_ONLY,
@@ -78,6 +85,7 @@ int main(int argc, char** argv)
 	ARRAY_LENGTH * sizeof(int), NULL, NULL);
 	bufC = clCreateBuffer(context, CL_MEM_READ_WRITE,
 	ARRAY_LENGTH * sizeof(int), NULL, NULL);
+
 	/* Transferência dos arrays de entrada para a memória
 	do dispositivo */
 	clEnqueueWriteBuffer(queue, bufA, CL_TRUE, 0,
@@ -86,24 +94,30 @@ int main(int argc, char** argv)
 	clEnqueueWriteBuffer(queue, bufB, CL_TRUE, 0,
 	ARRAY_LENGTH * sizeof(int), hostB, 0,
 	NULL, NULL);
+
 	/* Configuração dos argumentos do kernel */
 	clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufA);
 	clSetKernelArg(kernel, 1, sizeof(cl_mem), &bufB);
 	clSetKernelArg(kernel, 2, sizeof(cl_mem), &bufC);
+
 	/* Envio do kernel para execução no dispositivo */
 	clEnqueueNDRangeKernel(queue, kernel, 1, NULL,
 	globalSize, NULL, 0, NULL, NULL);
+
 	/* Sincronização (bloqueia hospedeiro até término da
 	execução do kernel */
 	clFinish(queue);
+
 	/* Transferência dos resultados da computação para a
 	memória do hospedeiro */
 	clEnqueueReadBuffer(queue, bufC, CL_TRUE, 0,
 	ARRAY_LENGTH * sizeof(int), hostC, 0,
 	NULL, NULL);
+
 	/* Impressão dos resultados na saída padrão */
 	for (i = 0; i < ARRAY_LENGTH; ++i)
 	printf("%d - %d = %d\n", hostA[i], hostB[i], hostC[i]);
+
 	/* Liberação de recursos e encerramento da aplicação */
 	clReleaseMemObject(bufA);
 	clReleaseMemObject(bufB);
